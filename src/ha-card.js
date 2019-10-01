0d551db42599635e674125bc15009ffb2c49823c
Thomas Lovén <thomasloven@gmail.com>
2019-10-01 21:18:17 +0200

    Add entities card tap_action

---
+++ src/ha-card.js
@@ -0,0 +1,48 @@
+import {fireEvent} from "/card-tools/event.js";
+import {applyStyle} from "./apply-style.js";
+
+customElements.whenDefined('ha-card').then(() => {
+  const HaCard = customElements.get('ha-card');
+
+  const findConfig = function(node) {
+    if(node.config)
+      return node.config;
+    if(node._config)
+      return node._config;
+    if(node.host)
+      return findConfig(node.host);
+    if(node.parentElement)
+      return findConfig(node.parentElement);
+    if(node.parentNode)
+      return findConfig(node.parentNode);
+    return null;
+  };
+
+
+  HaCard.prototype.firstUpdated = function() {
+    // Move the header inside the slot instead of in the shadowDOM
+    // makes it easier to style it consistently
+    const header = this.shadowRoot.querySelector(".card-header");
+    if(header)
+    {
+      this.insertBefore(header, this.children[0]);
+    }
+
+    const config = findConfig(this);
+    if(!config || !config.style) return;
+
+    let entity_ids = config.entity_ids;
+
+    const apply = () => {
+      applyStyle(this, config.style, {
+          variables: {config},
+          entity_ids
+        }, !!config.debug_cardmod);
+    }
+
+    apply();
+    window.addEventListener("location-changed", () => apply());
+  }
+
+  fireEvent('ll-rebuild', {});
+});
---
+++ src/hui-glance-card.js
@@ -0,0 +1,57 @@
+import {fireEvent} from "/card-tools/event.js";
+import {applyStyle} from "./apply-style.js";
+
+customElements.whenDefined('hui-glance-card').then(() => {
+  const GlanceCard = customElements.get('hui-glance-card');
+
+  GlanceCard.prototype.firstUpdated = function () {
+    const entities = this.shadowRoot.querySelectorAll("ha-card div.entity");
+    entities.forEach((e) => {
+      const root = e.attachShadow({mode: "open"});
+      [...e.children].forEach((el) => root.appendChild(el));
+      const styletag = document.createElement("style");
+      root.appendChild(styletag);
+      styletag.innerHTML = `
+      :host {
+        box-sizing: border-box;
+        padding: 0 4px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        cursor: pointer;
+        margin-bottom: 12px;
+        width: var(--glance-column-width, 20%);
+      }
+      div {
+        width: 100%;
+        text-align: center;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+      .name {
+        min-height: var(--paper-font-body1_-_line-height, 20px);
+      }
+      state-badge {
+        margin: 8px 0;
+      }
+      `;
+
+      const config = e.entityConf;
+      if(!config.style) return;
+      let entity_ids = config.entity_ids;
+
+      const apply = () => {
+        applyStyle(root, config.style, {
+            variables: {config},
+            entity_ids
+          }, !!config.debug_cardmod);
+      }
+
+      apply();
+      window.addEventListener("location-changed", apply);
+    });
+  }
+
+  fireEvent('ll-rebuild', {});
+});
