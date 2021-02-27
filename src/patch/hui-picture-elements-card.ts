3c7992e0550d781c45e07d51c8af72df0dbdb2bb
Mattias Persson <36163594+matt8707@users.noreply.github.com>
2021-02-27 18:44:04 +0100

    fix typo classList (#106)

--- src/patch/hui-picture-elements-card.ts
+++ src/patch/hui-picture-elements-card.ts
@@ -14,7 +14,7 @@ customElements.whenDefined("hui-picture-elements-card").then(() => {
 
     for (const [i, el] of this._elements.entries()) {
       const config = this._config.elements[i];
-      if (config?.card_mod?.class) el.clasList.add(config.card_mod.class);
+      if (config?.card_mod?.class) el.classList.add(config.card_mod.class);
       if (config?.type)
         el.classList.add(`type-${config.type.replace(":", "-")}`);
       applyToElement(el, "element", config?.card_mod?.style, { config });
