bb0dbffcb21367b4d78cd063c437b5bc2a4f9668
dcapslock <dcapes-davis@outlook.com.au>
2025-10-10 04:55:45 +0000

    Simplify config panel patching

--- src/patch/ha-panel-config.ts
+++ src/patch/ha-panel-config.ts
@@ -1,55 +1,45 @@
-import { patch_element, patch_elements } from "../helpers/patch_function";
+import { patch_element } from "../helpers/patch_function";
 import { ModdedElement, apply_card_mod } from "../helpers/apply_card_mod";
-import { LitElement } from "lit";
 
 /*
-Patch ha-config-* for theme styling
+Patch ha-panel-config for theme styling
+Config panels are routed via removing last Child and adding a new one.
+Hence we need to prepend card_mod element to not interfere with the routing.
 
 There is no style passed to apply_card_mod here, everything comes only from themes.
 
 This will only work if card-mod loaded as a Frontend module.
 */
 
-const config_elements = [
-  "ha-config-dashboard",
-  "ha-config-devices-dashboard",
-  "ha-config-device-page",
-  "ha-config-entities",
-  "ha-config-helpers",
-  "ha-config-integrations-dashboard",
-  "ha-config-integration-page",
-  "ha-config-cloud",
-  "ha-config-automation",
-  "ha-config-scene",
-  "ha-config-script",
-  "ha-config-blueprint",
-  "ha-config-areas-dashboard",
-  "ha-config-labels",
-  "ha-config-zone",
-  "ha-config-lovelace-dashboards",
-  "ha-config-voice-assistants-assistants",
-  "ha-config-tags",
-  "ha-config-person",
-  "ha-config-users",
-  "ha-config-info",
-  "ha-config-system-navigation",
-  "ha-config-section-general",
-  "ha-config-section-updates",
-  "ha-config-repairs-dashboard",
-  "ha-config-logs",
-  "ha-config-backup",
-  "ha-config-section-analytics",
-  "ha-config-section-network",
-  "dhcp-config-panel",
-  "ssdp-config-panel",
-  "zeroconf-config-panel",
-  "ha-config-hardware",
-];
-
-@patch_elements(config_elements)
+@patch_element("ha-panel-config")
 class HaConfigPatch extends ModdedElement {
   updated(_orig, ...args) {
     _orig?.(...args);
-    apply_card_mod(this, "config");
+    apply_card_mod(this, "config", { prepend: true });
+  }
+}
+
+/*
+Patch ha-panel-custom
+*/
+
+@patch_element("ha-panel-custom")
+class HaPanelCustomPatch extends ModdedElement {
+  updated(_orig, ...args) {
+    _orig?.(...args);
+    apply_card_mod(this, "panel-custom", { prepend: true });
+  }
+}
+
+/* Patch ha-top-app-bar-fixed for theme styling
+This is needed to best style the top app bar in the config panel.
+The ultimate background styling for config panels come from this element.
+*/
+
+@patch_element("ha-top-app-bar-fixed")
+class HaTopAppBarFixedPatch extends ModdedElement {
+  updated(_orig, ...args) {
+    _orig?.(...args);
+    apply_card_mod(this, "top-app-bar-fixed");
   }
 }
