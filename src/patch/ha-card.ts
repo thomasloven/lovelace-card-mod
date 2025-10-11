0b38780c997320e99468643c07e901dffcc85733
dcapslock <dcapes-davis@outlook.com.au>
2025-10-11 12:43:50 +1100

    Add back ha-card patch for cards not wrapped by hui-card (#476)
    * hui-card hui-badge (standard) and ha-card for others.
    
    * hui-card hui-badge (standard) and ha-card for others.
    
    * Fixup merge issue with yaml

---
+++ src/patch/ha-card.ts
@@ -0,0 +1,55 @@
+import { patch_element, patch_object } from "../helpers/patch_function";
+import { apply_card_mod } from "../helpers/apply_card_mod";
+import { ModdedElement } from "../helpers/apply_card_mod";
+
+/*
+Patch the ha-card element to on first update:
+- if it's parent is a hui-card, do nothing (as that is already handled in hui-card patch)
+- try to find the config parameter of it's parent element
+- Apply card_mod styles according to that config
+*/
+
+@patch_element("ha-card")
+class HaCardPatch extends ModdedElement {
+  _cardMod = [];
+  async firstUpdated(_orig, ...args) {
+    await _orig?.(...args);
+
+    const huiCard = (this.parentNode as any)?.host?.parentNode;
+    if (huiCard && huiCard.localName === "hui-card") return;
+
+    const config = findConfig(this);
+    if (!config) return;
+
+    const cls = `type-${config?.type?.replace?.(":", "-")}`;
+    await apply_card_mod(
+      this,
+      "card",
+      config?.card_mod,
+      { config },
+      false,
+      cls
+    );
+
+    const parent = (this.parentNode as any)?.host;
+    if (!parent) return;
+
+    patch_object(parent, ModdedElement);
+    parent._cardMod = this._cardMod;
+  }
+}
+
+interface LovelaceCard extends Node {
+  config?: any;
+  _config?: any;
+  host?: LovelaceCard;
+}
+
+export function findConfig(node: LovelaceCard) {
+  if (node.config) return node.config;
+  if (node._config) return node._config;
+  if (node.host) return findConfig(node.host);
+  if (node.parentElement) return findConfig(node.parentElement);
+  if (node.parentNode) return findConfig(node.parentNode);
+  return null;
+}
