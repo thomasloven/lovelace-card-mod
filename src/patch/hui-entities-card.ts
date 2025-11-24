8258a57dc65b6e1c35d0c5a791bc8eea1c4e3705
dcapslock <dcapes-davis@outlook.com.au>
2025-11-24 19:02:48 +1100

    Fix conditional row patching by not patching the parent row which can leak styles and introducing for the row itself

--- src/patch/hui-entities-card.ts
+++ src/patch/hui-entities-card.ts
@@ -11,13 +11,14 @@ Patch the hui-entities-card specifically in order to handle individual styling o
 class HuiEntitiesCardPatch extends ModdedElement {
   _renderEntity(_orig, config, ...rest) {
     const retval = _orig?.(config, ...rest);
-    if (config?.type === "custom:mod-card") return retval;
+    if (config?.type === "custom:mod-card" || config?.type === "conditional") return retval;
 
     if (!retval?.values) return retval;
     const row = retval.values[1];
     if (!row) return retval;
 
-    const cls = `type-${config?.type?.replace?.(":", "-")}`;
+    const cls = config?.type ? 
+      `type-${config.type.replace?.(":", "-")}` : "type-entity";
     const apply = async () => {
       await await_element(row);
       patch_object(row, ModdedElement);
@@ -30,3 +31,31 @@ class HuiEntitiesCardPatch extends ModdedElement {
     return retval;
   }
 }
+
+/*
+Patch conditional row specifically as it creates rows dynamically
+*/
+
+@patch_element("hui-conditional-row")
+class HuiConditionalRowPatch extends ModdedElement {
+  _element;
+  
+  setConfig(_orig, config, ...args) {
+    _orig?.(config, ...args);
+    const row = this._element;
+    if (!row) return;
+    if (!config?.row || config?.row?.type === "custom:mod-card") return;
+    
+    const cls = config?.row?.type ? 
+    `type-${config.row.type.replace?.(":", "-")}` : "type-entity";
+    const apply = async () => {
+      await await_element(row);
+      patch_object(row, ModdedElement);
+      apply_card_mod(row, "row", config.row.card_mod, { config: config.row }, true, cls);
+      row.addEventListener("ll-rebuild", apply);
+    };
+
+    Promise.all([this.updateComplete]).then(() => apply());
+  }
+}
+
