5187d43295435fefa9261e22fda48db851278794
dcapslock <dcapes-davis@outlook.com.au>
2025-11-27 11:07:32 +1100

    Apply card-mod to elements of conditional picture elements card (#538)
    * Apply card-mod to elements of conditional picture elements card
    
    * Implement code review changes
    
    * Further changes after code review

--- src/patch/hui-picture-elements-card.ts
+++ src/patch/hui-picture-elements-card.ts
@@ -3,7 +3,7 @@ import { patch_element, patch_object } from "../helpers/patch_function";
 import { await_element } from "../helpers/selecttree";
 
 /*
-Patch the hui-epicture-elements-card specifically in order to handle individual styling of each element
+Patch the hui-picture-elements-card specifically in order to handle individual styling of each element
 */
 
 @patch_element("hui-picture-elements-card")
@@ -11,12 +11,36 @@ class PictureElementsCardPatch extends ModdedElement {
   setConfig(_orig, ...args) {
     _orig?.(...args);
 
-    for (const [i, el] of (this as any)._elements.entries()) {
-      await_element(el);
-      patch_object(el, ModdedElement);
-      const config = (this as any)._config.elements[i];
-      const cls = `type-${config?.type?.replace?.(":", "-")}`;
-      apply_card_mod(el, "element", config?.card_mod, { config }, true, cls);
+    const apply = async () => { 
+      for (const [i, el] of (this as any)._elements.entries()) {
+        await await_element(el);
+        patch_object(el, ModdedElement);
+        const config = (this as any)._config.elements[i];
+        const cls = `type-${config?.type?.replace?.(":", "-")}`;
+        apply_card_mod(el, "element", config?.card_mod, { config }, true, cls);
+      }
     }
+    Promise.all([this.updateComplete]).then(() => apply());
+  }
+}
+
+/*
+Patch conditional element specifically as it creates elements dynamically
+*/
+
+@patch_element("hui-conditional-element")
+class HuiConditionalElementPatch extends ModdedElement {
+  setConfig(_orig, ...args) {
+    _orig?.(...args);
+    const apply = async () => {
+      for (const [i, el] of (this as any)._elements.entries()) {
+        await await_element(el);
+        patch_object(el, ModdedElement);
+        const config = (this as any)._config.elements[i];
+        const cls = `type-${config?.type?.replace?.(":", "-")}`;
+        apply_card_mod(el, "element", config?.card_mod, { config }, true, cls);
+      }
+    }
+    Promise.all([this.updateComplete]).then(() => apply());
   }
 }
