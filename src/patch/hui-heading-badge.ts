1233af9c9026a5e20a53e96e4016ca87c95dcbdf
dcapslock <dcapes-davis@outlook.com.au>
2025-11-30 22:59:07 +1100

    Add styling of heading badges

---
+++ src/patch/hui-heading-badge.ts
@@ -0,0 +1,36 @@
+import { patch_element, patch_object } from "../helpers/patch_function";
+
+import { apply_card_mod } from "../helpers/apply_card_mod";
+import { ModdedElement } from "../helpers/apply_card_mod";
+
+@patch_element("hui-heading-badge")
+class HuiBadgePatch extends ModdedElement {
+  _element: ModdedElement;
+  config;
+
+  async _add_card_mod() {
+    if (!this._element) return;
+    
+    const cls = `type-${this.config?.type?.replace?.(":", "-")}`;
+
+    await apply_card_mod(
+      this._element,
+      "heading-badge",
+      this.config.card_mod,
+      { config: this.config },
+      true,
+      cls
+    );
+    this._cardMod = this._element._cardMod;
+  }
+
+  _loadElement(_orig, ...args) {
+    _orig?.(...args);
+    this._add_card_mod();
+  }
+
+  _updateElement(_orig, ...args) {
+    _orig?.(...args);
+    this._add_card_mod();
+  }
+}
